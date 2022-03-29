49306e0b665c178e0f961b97641881033f56d688
BharathiPravallika <52744796+BharathiPravallika@users.noreply.github.com>
2022-03-29 17:53:35 +0530

    Create P10DataTypeDetails.java

---
+++ P10DataTypeDetails.java
@@ -0,0 +1,14 @@
+import java.lang.*;
+import javax.swing.*;
+import java.awt.*;
+
+class P10DataTypeDetails 
+{
+    public static void main(String args[])
+    {
+        int x = 5;
+        int y = -5;
+        System.out.println(Integer.toBinaryString(x));
+        System.out.println(Integer.toBinaryString(y));
+    }
+}
