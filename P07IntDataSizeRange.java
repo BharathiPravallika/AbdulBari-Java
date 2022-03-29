3d5dfa287d336c7bc78affaac80922e2f88b2f85
BharathiPravallika <52744796+BharathiPravallika@users.noreply.github.com>
2022-03-29 17:47:50 +0530

    Create P07IntDataSizeRange.java

---
+++ P07IntDataSizeRange.java
@@ -0,0 +1,10 @@
+import java.lang.*;
+class P07IntDataSizeRange 
+{
+    public static void main(String args[])
+    {
+        System.out.println("int Min:" + Integer.MIN_VALUE);
+        System.out.println("int Max:" + Integer.MAX_VALUE);
+        System.out.println("int Bytes:" + Integer.BYTES);
+    }
+}
