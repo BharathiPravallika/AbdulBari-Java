b4e8fc0a10337fd7b307db6f7a970ea91d8546cd
BharathiPravallika <52744796+BharathiPravallika@users.noreply.github.com>
2022-03-29 17:57:11 +0530

    Create P12Arithmetic.java

---
+++ P12Arithmetic.java
@@ -0,0 +1,22 @@
+import java.lang.*;
+class P12Arithmetic 
+{
+    public static void main(String args[])
+    {
+        int a = 14, b = 5;
+        float c = (float)a/b;
+        int d = a/b;
+        // int r = a%b;
+        
+        float f1 = 14.3f, f2 = 3.2f;
+        float r = a%b;
+        
+        
+        System.out.println(c);
+        System.out.println(d);
+        // System.out.println(r);
+        
+        System.out.println(f1);
+        System.out.println(r);
+    }
+}
