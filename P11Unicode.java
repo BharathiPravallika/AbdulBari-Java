821b02da7726c05b33fb64fe7c2097471c15feb4
BharathiPravallika <52744796+BharathiPravallika@users.noreply.github.com>
2022-03-29 17:55:35 +0530

    Create P11Unicode.java

---
+++ P11Unicode.java
@@ -0,0 +1,9 @@
+import java.lang.*;
+class P11Unicode 
+{
+    public static void main(String args[])
+    {
+        for(char c = 0X0C00; c<= 0X0C7F; c++)
+        System.out.print(c+" ");
+    }
+}
