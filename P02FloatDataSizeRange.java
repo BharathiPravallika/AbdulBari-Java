6c6700767c3fc6c54ffd3f5a578c997ec7a5f152
BharathiPravallika <52744796+BharathiPravallika@users.noreply.github.com>
2022-03-29 17:38:16 +0530

    Create P02FloatDataSizeRange.java

---
+++ P02FloatDataSizeRange.java
@@ -0,0 +1,10 @@
+import java.lang.*;
+class P02FloatDataSizeRange 
+{
+    public static void main(String args[])
+    {
+        System.out.println("Float Min:" + Float.MIN_VALUE);
+        System.out.println("Float Max:" + Float.MAX_VALUE);
+        System.out.println("Float Bytes:" + Float.BYTES);
+    }
+}
