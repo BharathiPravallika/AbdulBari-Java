714675d283b242ff714ccc8e9cfdfa06ce8b2155
BharathiPravallika <52744796+BharathiPravallika@users.noreply.github.com>
2022-03-29 17:49:54 +0530

    Create P08Variables.java

---
+++ P08Variables.java
@@ -0,0 +1,33 @@
+import java.lang.*;
+class P08Variables 
+{
+    public static void main(String args[])
+    {
+        byte B = 10;
+        System.out.println(B);
+        B = 25;
+        System.out.println(B);
+    }
+}
+
+-------------------------------------------
+
+import java.lang.*;
+class P08Variables 
+{
+    public static void main(String args[])
+    {
+        byte B = 127;
+        short S = 300;
+        int i = 100;
+        
+        float f = 25.3f;
+        char c = 'A';
+        
+        System.out.println(B);
+        System.out.println(S);
+        System.out.println(i);
+        System.out.println(f);
+        System.out.println(c);
+    }
+}
