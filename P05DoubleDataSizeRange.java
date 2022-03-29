b4b984dc1fea73fc78a6efb2509dc484151eac44
BharathiPravallika <52744796+BharathiPravallika@users.noreply.github.com>
2022-03-29 17:44:00 +0530

    Create P05DoubleDataSizeRange.java

---
+++ P05DoubleDataSizeRange.java
@@ -0,0 +1,10 @@
+import java.lang.*;
+class P05DoubleDataSizeRange 
+{
+    public static void main(String args[])
+    {
+        System.out.println("Double Min:" + Double.MIN_VALUE);
+        System.out.println("Double Max:" + Double.MAX_VALUE);
+        System.out.println("Double Bytes:" + Double.BYTES);
+    }
+}
