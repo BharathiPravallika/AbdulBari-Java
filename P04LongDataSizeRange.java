fee153237044ebb721692467704328e9a8d4fe01
BharathiPravallika <52744796+BharathiPravallika@users.noreply.github.com>
2022-03-29 17:42:21 +0530

    Create P04LongDataSizeRange.java

---
+++ P04LongDataSizeRange.java
@@ -0,0 +1,10 @@
+import java.lang.*;
+class P04LongDataSizeRange 
+{
+    public static void main(String args[])
+    {
+        System.out.println("Long Min:" + Long.MIN_VALUE);
+        System.out.println("Long Max:" + Long.MAX_VALUE);
+        System.out.println("Long Bytes" + Long.BYTES);
+    }
+}
