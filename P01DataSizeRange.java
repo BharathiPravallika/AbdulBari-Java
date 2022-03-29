b5553fb64799545b9994dae37bcee9461a0d7367
BharathiPravallika <52744796+BharathiPravallika@users.noreply.github.com>
2022-03-29 17:34:40 +0530

    Create P01DataSizeRange.java

---
+++ P01DataSizeRange.java
@@ -0,0 +1,10 @@
+import java.lang.*;
+class P01DataSizeRange
+{
+    public static void main(String args[])
+    {
+        System.out.println("Byte Min:" + Byte.MIN_VALUE);
+        System.out.println("Byte Max:" + Byte.MAX_VALUE);
+        System.out.println("Byte Byte "+Byte.BYTES);
+    }
+}
