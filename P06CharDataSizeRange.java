d164a2bfa9cfed38870ae0987063b26e5e6e3624
BharathiPravallika <52744796+BharathiPravallika@users.noreply.github.com>
2022-03-29 17:45:58 +0530

    Create P06CharDataSizeRange.java

---
+++ P06CharDataSizeRange.java
@@ -0,0 +1,10 @@
+import java.lang.*;
+class P06CharDataSizeRange 
+{
+    public static void main(String args[])
+    {
+        System.out.println("Char Min:" + (int)Character.MIN_VALUE);
+        System.out.println("Char Max:" + (int)Character.MAX_VALUE);
+        System.out.println("Char Bytes:" + Character.BYTES);
+    }
+}
