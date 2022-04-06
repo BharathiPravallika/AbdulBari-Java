11e30b35cfa59f0d92f786ef844c90c1d937d3b9
BharathiPravallika <52744796+BharathiPravallika@users.noreply.github.com>
2022-04-06 14:24:46 +0530

    Update and rename P34.java to P34TypeOfWebsiteAndProtocol.java

--- P34TypeOfWebsiteAndProtocol.java
+++ P34TypeOfWebsiteAndProtocol.java
@@ -1,6 +1,6 @@
 import java.lang.*;
 import java.util.*;
-class TypeOfWebsiteAndProtocol 
+class P34TypeOfWebsiteAndProtocol 
 {
     public static void main(String args[]){
         Scanner sc = new Scanner(System.in);
