1e47bfca22965f01ca73984b16645b01d69eb8eb
BharathiPravallika <52744796+BharathiPravallika@users.noreply.github.com>
2022-04-06 16:39:27 +0530

    Create P35SwitchCase.java

---
+++ P35SwitchCase.java
@@ -0,0 +1,21 @@
+import java.lang.*;
+import java.util.*;
+class P35SwitchCase 
+{
+    public static void main(String args[])
+    {
+        int n=3;
+        switch(n)
+        {
+            case 1: System.out.println("One");   
+                break;
+            case 2: System.out.println("Two");
+                break;
+            case 3: System.out.println("Three");
+                break;
+            case 4: System.out.println("Four");
+                break;
+            default : System.out.println("Invalid");
+        }
+    }
+}
