1eb1328e96647018e62aaac96a13de5678c41da8
BharathiPravallika <52744796+BharathiPravallika@users.noreply.github.com>
2022-04-13 14:39:41 +0530

    Create P41DisplayDigitsInNewLine.java

---
+++ P41DisplayDigitsInNewLine.java
@@ -0,0 +1,15 @@
+import java.lang.*;
+import java.util.*;
+class P41DisplayDigitsInNewLine 
+{
+    public static void main(String args[]){
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        while(n>0){
+            int r = n%10;
+            n = n/10;
+            System.out.println(r);
+        }
+            
+    }
+}
