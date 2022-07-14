e9d613337e25f7ce9b4c292e5d13c236e1cdf119
BharathiPravallika <52744796+BharathiPravallika@users.noreply.github.com>
2022-07-14 05:46:44 +0530

    Create P49_leftRotationofArray.java

---
+++ P49_leftRotationofArray.java
@@ -0,0 +1,18 @@
+import java.lang.*;
+import java.util.*;
+public class Main{
+    public static void main(String args[]){
+        int A[] = {5, 9, 6, 10, 12, 7, 3, 5, 4, 2};
+        for(int x:A)
+            System.out.print(x + ",");
+        System.out.println("");
+        int temp = A[0];
+        for(int i=1; i<A.length; i++){
+            A[i-1] = A[i];
+        }
+        A[A.length-1] = temp;
+        for(int x:A)
+            System.out.print(x+",");
+        System.out.println("");
+    }
+}
